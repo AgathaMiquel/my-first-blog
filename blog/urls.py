a481c3584527ae81502283660d37b3849ab24610
agatha <agatha.miquel@gmail.com>
2016-06-04 19:11:28 +0200

    Ajout de vues qui permettent de créer et d'éditer un post de blog sur le site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^/uneautrepage', views.testpage, name='testpage')
+    url(r'^uneautrepage', views.testpage, name='testpage')
 ]
